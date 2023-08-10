26cfa0609856b556acbff2972e3a23885a56224e
Ddilibe <franklinfidelugwuowo@gmail.com>
2023-08-10 18:26:05 +0100

    [Chore]: Added the models configurations in the car.py file

--- car.py
+++ car.py
@@ -17,7 +17,10 @@ from models.engine.sternman import Sternman
 from models.engine.wiloughby import Wiloughby
 from models.engine.engine import Engine
 from models.battery.battery import Battery
-from typing import TypeAlias
+from models.tire.tire import Tire
+from models.tire.carrigan import Carrigan
+from models.tire.octoprime import Octoprime
+from typing import TypeAlias, List
 from datetime import date
 import pdb
 
@@ -25,12 +28,13 @@ import pdb
 
 bat:TypeAlias = Battery
 eng:TypeAlias = Engine
+tir:TypeAlias = Tire
 
 def create_car(name: str, *args: list):
     # pdb.set_trace()
     class Car():
-        def __init__(self, battery: bat, engine: eng, *args, **kwargs):
-            self.battery, self.engine = battery, engine
+        def __init__(self, battery: bat, engine: eng, tire: tir, *args, **kwargs):
+            self.battery, self.engine, self.tire = battery, engine, tire
             self.start_car(name, *args)
 
         def start_car(self, string: str, *args):
@@ -42,37 +46,41 @@ def create_car(name: str, *args: list):
             car = cars.get(string)(*args)
             return car
 
-        def create_calliope(self, current_date: date, last_service_date: date, current_mileage: int, last_service_mileage: int):
+        def create_calliope(self, current_date: date, last_service_date: date, current_mileage: int, last_service_mileage: int, tire_sensor: List):
             self.battery = self.battery(last_service_date, current_date)
             self.engine = self.engine(last_service_mileage, current_mileage)
+            self.tire = self.tire(tire_sensor)
 
-        def create_gilssage(self, current_date: date, last_service_date: date, current_mileage: int, last_service_mileage: int):
+        def create_gilssage(self, current_date: date, last_service_date: date, current_mileage: int, last_service_mileage: int, tire_sensor: List):
             self.battery = self.battery(last_service_date, current_date)
             self.engine = self.engine(last_service_mileage, current_mileage)
+            self.tire = self.tire(tire_sensor)
 
-        def create_palindrome(self, current_date: date, last_service_date: date, warning_light_on: bool):
+        def create_palindrome(self, current_date: date, last_service_date: date, warning_light_on: bool, tire_sensor: List):
             self.battery = self.battery(last_service_date, current_date)
             self.engine = self.engine(warning_light_on)
+            self.tire = self.tire(tire_sensor)
 
-        def create_rorschach(self, current_date: date, last_service_date: date, current_mileage: int, last_service_mileage: int):
+        def create_rorschach(self, current_date: date, last_service_date: date, current_mileage: int, last_service_mileage: int, tire_sensor: List):
             self.battery = self.battery(last_service_date, current_date)
             self.engine = self.engine(last_service_mileage, current_mileage)
+            self.tire = self.tire(tire_sensor)
 
-        def create_thovex(self, current_date: date, last_service_date: date, current_mileage: int, last_service_mileage: int):
+        def create_thovex(self, current_date: date, last_service_date: date, current_mileage: int, last_service_mileage: int, tire_sensor: List):
             self.battery = self.battery(last_service_date, current_date)
             self.engine = self.engine(last_service_mileage, current_mileage)
+            self.tire = self.tire(tire_sensor)
 
         def needs_service(self):
-            if self.battery.needs_service() or self.engine.needs_service():
-                return True
-            return False
+            return self.battery.needs_service() or self.engine.needs_service() or self.tire.needs_service()
+
 
     cars ={
-        "calliope": (Spindler, Capulet),
-        "gilssade": (Spindler, Wiloughby),
-        "palindrome": (Spindler, Sternman),
-        "rorschach": (Nubbin, Wiloughby),
-        "thovex": (Nubbin, Capulet),
+        "calliope": (Spindler, Capulet, Carrigan),
+        "gilssade": (Spindler, Wiloughby, Octoprime),
+        "palindrome": (Spindler, Sternman, Carrigan),
+        "rorschach": (Nubbin, Wiloughby, Octoprime),
+        "thovex": (Nubbin, Capulet, Carrigan),
     }
     # pdb.set_trace()
     new_car = Car(*cars.get(name), *args)
